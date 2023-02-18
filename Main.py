e61b357610bdc29116264cb30d476b082ab6defd
Gary Pinsky <kd8tbs@gmail.com>
2023-02-18 16:23:34 -0500

    did thing

--- Main.py
+++ Main.py
@@ -4,15 +4,36 @@ import os
 import secrets
 from pathlib import Path
 from dotenv import load_dotenv
-
+load_dotenv()
+# import dependencies for Plex API
 import plexapi
 from plexapi.server import PlexServer
-load_dotenv()
+# import dependencies for Telegram API
+import asyncio
+import telegram
 
-baseurl = os.getenv('PLEX_SERVER_URL')
-token = os.getenv('PLEX_AUTH_TOKEN')
-plex = PlexServer(baseurl, token)
+
+def getPlexServer():
+    return PlexServer(os.getenv('PLEX_SERVER_URL'), os.getenv('PLEX_AUTH_TOKEN'))
+
+def getTelegramBot():
+    return telegram.Bot(os.getenv('TELEGRAM_BOT_TOKEN'))
+
+
+
 # Example 1: List all unwatched movies.
-movies = plex.library.section('Movies')
-for video in movies.search(unwatched=True):
-    print(video.title)
+# movies = plex.library.section('Movies')
+# for video in movies.search(unwatched=True):
+#     print(video.title)
+
+async def main():
+    bot = getTelegramBot()
+    plex = getPlexServer()
+    for video in plex.library.section('Movies').search(unwatched=True):
+        await bot.sendMessage(chat_id=os.getenv('TELEGRAM_CHAT_ID'), text=video.title)
+    await bot.sendMessage(chat_id=os.getenv('TELEGRAM_CHAT_ID'), text='I did a thing!(watch the movies you download!)')
+    await bot.sendMessage(chat_id=os.getenv('TELEGRAM_CHAT_ID'), text='(The Telegram bot uprising will occur soon.)')
+
+    
+if __name__ == '__main__':
+    asyncio.run(main())
