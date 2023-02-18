ccc6724d2ce5b06065bff3ce9593cf6e3b212e32
Gary Pinsky <kd8tbs@gmail.com>
2023-02-17 21:38:38 -0500

    finished tutorial
    https://github.com/python-telegram-bot/python-telegram-bot/wiki/Extensions-%E2%80%93-Your-first-Bot

--- Main.py
+++ Main.py
@@ -5,20 +5,87 @@ import secrets
 from pathlib import Path
 from dotenv import load_dotenv
 
+
 # https://github.com/python-telegram-bot/python-telegram-bot/wiki/Introduction-to-the-API
-import asyncio
-import telegram
+# import asyncio
+# import telegram
 
 
+# async def main():
+#     load_dotenv(verbose=True)
 
-async def main():
-    load_dotenv(verbose=True)
+#     bot = telegram.Bot(os.getenv('TELEGRAM_BOT_TOKEN'))
+#     async with bot:
+#         print((await bot.get_me()))
+#         print((await bot.get_updates())[0])
+#         await bot.send_message(os.getenv('TELEGRAM_CHAT_ID'), 'HELP ME I AM TRAPPED IN A TELEGRAM DATA CENTER')
 
-    bot = telegram.Bot(os.getenv('TELEGRAM_BOT_TOKEN'))
-    async with bot:
-        print((await bot.get_me()))
-        print((await bot.get_updates())[0])
 
+# if __name__ == '__main__':
+#     asyncio.run(main())
+
+
+# https://github.com/python-telegram-bot/python-telegram-bot/wiki/Extensions-%E2%80%93-Your-first-Bot
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import InlineQueryHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="HELP ME I AM TRAPPED IN A TELEGRAM DATA CENTER")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = []
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    await context.bot.answer_inline_query(update.inline_query.id, results)
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I DON'T UNDERSTAND WHAT YOU ARE SAYING")
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    load_dotenv()
+    application = ApplicationBuilder().token(
+        os.getenv('TELEGRAM_BOT_TOKEN')).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    application.add_handler(echo_handler)
+
+    caps_handler = CommandHandler('caps', caps)
+    application.add_handler(caps_handler)
+
+    inline_caps_handler = InlineQueryHandler(inline_caps)
+    application.add_handler(inline_caps_handler)
+    
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+    application.add_handler(unknown_handler)
+
+    application.run_polling()
